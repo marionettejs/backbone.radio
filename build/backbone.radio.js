ae556624bd509fd96ab67fb92599655fc83b1ec8
Andrew Humphreys <ahumphreys87@googlemail.com>
2016-03-31 23:21:40 +0100

    Bump and Build 2.0.0-pre.1

--- build/backbone.radio.js
+++ build/backbone.radio.js
@@ -1,4 +1,4 @@
-// Backbone.Radio v1.0.4
+// Backbone.Radio v2.0.0-pre.1
 
 (function (global, factory) {
   typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory(require('underscore'), require('backbone')) :
@@ -21,7 +21,7 @@
 
   var Radio = Backbone.Radio = {};
 
-  Radio.VERSION = '1.0.4';
+  Radio.VERSION = '2.0.0-pre.1';
 
   // This allows you to run multiple instances of Radio on the same
   // webapp. After loading the new version, call `noConflict()` to
